58c1f1c5cc0be64a645314dd493f3acf18890f16
brandonborn15 <btm1998x@gmail.com>
2021-04-30 17:02:29 -0500

    Create public class Leo123leo .java

---
+++ public class Leo123leo .java
@@ -0,0 +1,16 @@
+public class Leo123leo {
+    public static void main(String[] args){
+        if( [[ ! $1 =~ ^[A-Z]  && $1 =~ "[@]" ]] ){ 
+            echo "$1 is not a valid Java class name";
+            rm $1;
+            exit 2; 
+    }
+       }// main method
+}
+
+//Condition to validate the file name as a proper Java class name 
+
+//( Basically, make sure its first character is an upper case letter. ) 
+
+
+
